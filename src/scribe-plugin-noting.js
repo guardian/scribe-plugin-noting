95b9f495105aba0ab7c7d8a1cde760d24ad2cf10
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-08-05 12:40:36 +0100

    Updated with suggested changes

--- src/scribe-plugin-noting.js
+++ src/scribe-plugin-noting.js
@@ -11,9 +11,11 @@ define(function () {
     return function(scribe) {
 
       //currently
-      var tag = "span";
-      var nodeName = "SPAN";
+      var tag = "gu-note";
+      var nodeName = "GU-NOTE";
       var className = "note";
+      var dataName = "data-edited-by";
+      var dataDate = "data-edited-date";
       var blocks = ["P", "LI", "UL"];
 
       var noteCommand = new scribe.api.Command('insertHTML');
@@ -23,7 +25,8 @@ define(function () {
         var wrap = document.createElement(tag);
         wrap.className = className;
         var date = new Date().getTime().toString();
-        wrap.setAttribute("data-edited", user + " " + date);
+        wrap.setAttribute("data-edited-by", user);
+        wrap.setAttribute("data-edited-date", date);
         return wrap;
 
       }
@@ -42,14 +45,14 @@ define(function () {
         // we'll have to do more magic. So we append the
         // wrap to the element inside the block
         // there are some issues with LIs and Bs at the moment
-        var wrap = createWrap(wrap);
+        var wrap = createWrap();
         var temp = block.cloneNode(true);
         wrap.innerHTML = temp.innerHTML;
         temp.appendChild(wrap);
         return temp;
       }
 
-      function wrapRange(range, wrap) {
+      function wrapRange(range) {
         var temp = document.createDocumentFragment();
         var childNodes = range.childNodes;
 
@@ -58,15 +61,11 @@ define(function () {
         childNodes.forEach(function (item) {
           var tempNode;
 
-          if (!item) {
-            return;
-          }
-
           if(item.nodeType === Node.TEXT_NODE) {
             // this is for a basic selection
-            tempNode = wrapText(item, wrap);
+            tempNode = wrapText(item);
           } else {
-            tempNode = wrapBlock(item, wrap);
+            tempNode = wrapBlock(item);
           }
 
           temp.appendChild(tempNode);
@@ -76,20 +75,31 @@ define(function () {
       }
 
       function hasBlockElements (range) {
-        for (var i = 0, len = range.childNodes.length; i < len; i++) {
-          if(blocks.indexOf(range.childNodes[i].nodeName) !== -1) {
-            return true;
-          }
-          return false;
-        }
+        var childNodes = range.childNodes;
+
+        childNodes = Array.prototype.slice.call(childNodes);
+
+        var temp = childNodes.filter(function (item) {
+          return blocks.indexOf(item.nodeName) !== -1;
+        });
+
+        return temp.length > 0;
       }
 
       function checkScribeMarker (node) {
-        return (' ' + node.className + ' ').indexOf(' ' + "scribe-marker" + ' ') > -1;
+         return checkClass(node, "scribe-marker");
       }
 
+      function checkClass(node, value) {
+        if (!node.classList) {
+          return false;
+        }
+        return node.classList.contains(value);
+      }
+
+
       function isNote (node) {
-        return (' ' + node.className + ' ').indexOf(' ' + "note" + ' ') > -1;
+        return checkClass(node, "note");
       }
 
       function findScribeMarker(node) {
@@ -124,15 +134,6 @@ define(function () {
         func(node);
       }
 
-
-      function removeScribeMarkers (tree) {
-        walk(tree, function (node) {
-          if (checkScribeMarker(node)) {
-            node.parentElement.removeChild(node);
-          }
-        });
-      }
-
       function unwrap (element) {
         // get the innercontent and move it in place of the element
         // TODO: Only does a basic unwrap - does not do the inner
@@ -159,7 +160,7 @@ define(function () {
         var nodeList = [];
         walk(tree, function (node) {
 
-          if (done === true) {
+          if (done) {
             return; //do nothing
           }
 
@@ -203,10 +204,6 @@ define(function () {
 
 
         nodes.forEach(function (item, index, array) {
-          if (!item) {
-            return;
-          }
-
           var wrap;
           var parent = item.parentNode;
           var sibling = item.nextSibling;
@@ -229,56 +226,67 @@ define(function () {
         selection.selectMarkers();
       }
 
-
-      function iteratorWalk (commonAncestor, predicate) {
-        /*
-         * Basic algorithm
-         * Walk the DOM
-         * Find first scribe marker
-         * Wrap every element
-         * Find last scribe marker
-         * Return
-         */
-
-        var nodeIterator = document.createNodeIterator(commonAncestor, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT);
-        var nodeList = [];
-        var done = false;
-        var scribeMarkerLocated = false;
+      function basicUnwrap(selection, range) {
+        // this is a seriously flaky way of doing it at the moment
+        // I think there are much better alternatives
+        // TODO: Investigate if it's even worth doing this on an undo
+        // might just be able to use unwrap in the same way as it works
+        // when there are block elements.
 
 
+        // drop markers to play with the sibling
+        selection.placeMarkers();
+        selection.selectMarkers(true);
 
+        var commonAncestor = range.commonAncestorContainer;
+        var parent = commonAncestor.parentNode;
 
-        var node = nodeIterator.nextNode(); //skip the first element
-        while (node = nodeIterator.nextNode()) {
-          // when the count is 0 return
-
-          if (done === true) {
-            return; //do nothing
-          }
-
-          if (checkScribeMarker(node)) {
-            // begin pushing elements
-            if (scribeMarkerLocated === true) {
-              done = true;
-            } else {
-              scribeMarkerLocated = true;
-            }
-          }
-
-          if (!done && scribeMarkerLocated && predicate(node)) {
-            //scribe markers do not get pushed
-            nodeList.push(node);
-          }
+        // this is random - but basically the range thinks the
+        // span is the common ancestor if we only select a little bit of
+        // the note
+        if (commonAncestor.nextSibling) {
+          parent = commonAncestor.nextSibling.parentNode;
+        } else if (commonAncestor.previousSibling) {
+          parent = commonAncestor.previousSibling.parentNode;
         }
-        return nodeList;
+
+
+        var contents = document.createTextNode(commonAncestor.innerText);
+        parent.replaceChild(contents, commonAncestor);
+        selection.selectMarkers();
+
+
       }
+
+
+      function descentUnwrap (selection, range) {
+        // remove all styling from elements within the range
+        // in this case they have selected multiple nodes
+        selection.placeMarkers();
+        selection.selectMarkers(true);
+
+        // do a recursive unwrap
+        var commonAncestor = range.commonAncestorContainer;
+        var toBeUnWrapped = buildNodeList(commonAncestor, function (node) {
+          return isNote(node);
+        });
+
+        toBeUnWrapped = Array.prototype.slice.call(toBeUnWrapped);
+
+        toBeUnWrapped.forEach(function (item) {
+          unwrap(item);
+        });
+
+        selection.selectMarkers();
+
+      }
+
 
       noteCommand.execute = function () {
 
         var selection = new scribe.api.Selection();
         var range = selection.range;
-        var commonAncestor;
-
+        var cloned = range.cloneContents();
         // if the selection is the whole line, then we need to note the whole line
         // if it isn't then we just do the bit selected and nothing else.
         // selection.selection.data currently will duplicate things if there is no
@@ -287,63 +295,17 @@ define(function () {
 
           if (this.queryState()) {
 
-            if (!hasBlockElements(range.cloneContents())) {
-              // this is a seriously flaky way of doing it at the moment
-              // I think there are much better alternatives
-              // TODO: Investigate if it's even worth doing this on an undo
-              // might just be able to use unwrap in the same way as it works
-              // when there are block elements.
-
-
-              // drop markers to play with the sibling
-              selection.placeMarkers();
-              selection.selectMarkers(true);
-
-              commonAncestor = range.commonAncestorContainer;
-              var parent = commonAncestor.parentNode;
-
-              // this is random - but basically the range thinks the
-              // span is the common ancestor if we only select a little bit of
-              // the note
-              if (commonAncestor.nextSibling) {
-                parent = commonAncestor.nextSibling.parentNode;
-              } else if (commonAncestor.previousSibling) {
-                parent = commonAncestor.previousSibling.parentNode;
-              }
-
-
-              var contents = document.createTextNode(commonAncestor.innerText);
-              parent.replaceChild(contents, commonAncestor);
-              selection.selectMarkers();
-
+            if (!hasBlockElements(cloned)) {
+              basicUnwrap(selection, range);
             } else {
-              // remove all styling from elements within the range
-              // in this case they have selected multiple nodes
-              selection.placeMarkers();
-              selection.selectMarkers(true);
-
-              // do a recursive unwrap
-              commonAncestor = range.commonAncestorContainer;
-              var toBeUnWrapped = buildNodeList(commonAncestor, function (node) {
-                return isNote(node);
-              });
-
-              toBeUnWrapped = Array.prototype.slice.call(toBeUnWrapped);
-
-              toBeUnWrapped.forEach(function (item) {
-                unwrap(item);
-              });
-
-              selection.selectMarkers();
+              descentUnwrap(selection, range);
             }
-
-
           } else {
             // check if the selection has block elements.
             // if it does do the complex version,
             // otherwise do the simple version
-            if (!hasBlockElements(range.cloneContents())) {
-              var wrapped = wrapRange(range.cloneContents());
+            if (!hasBlockElements(cloned)) {
+              var wrapped = wrapRange(cloned);
               range.deleteContents();
               range.insertNode(wrapped);
             } else {
@@ -392,8 +354,8 @@ define(function () {
       /* There may be case when we don't want to use the default commands */
 
       scribe.el.addEventListener('keydown', function (event) {
-        //that's F10
-        if (event.keyCode === 121) {
+        //that's F10 and F8
+        if (event.keyCode === 121 ||event.keyCode === 119) {
           var noteCommand = scribe.getCommand("note");
           var selection = new scribe.api.Selection();
           var range = selection.range;
