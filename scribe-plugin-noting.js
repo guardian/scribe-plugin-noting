6e8eba9e3b79a749f9c577ba9940198256020e6c
Robin Edman <robin.edman@guardian.co.uk>
2014-10-08 15:37:41 +0100

    Now shows correct user again

--- scribe-plugin-noting.js
+++ scribe-plugin-noting.js
@@ -2,13 +2,11 @@
 
 var notingCommands = require('./src/noting-commands');
 
+// config, example:
+// { user: 'Firstname Lastname',
+//   scribeInstancesSelector: '.ui-rich-text-editor__input' }
 module.exports = function(config) {
   return function(scribe) {
-    var config = config || {
-        user: 'Unknown user',
-        scribeInstancesSelector: '.ui-rich-text-editor__input'
-    };
-
     notingCommands.init(scribe, config);
   };
 };
