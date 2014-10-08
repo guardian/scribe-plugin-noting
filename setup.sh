2214c73928e3a5ef686bf34f8712a7f552aac138
Simon Hildrew <simon@hildrew.net>
2014-10-08 11:32:20 +0100

    Don't download selenium if it is already there

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ bower install
 SELENIUM_VERSION=2.41.0
 SELENIUM_MINOR_VERSION=2.41
 mkdir -p vendor
-wget -O vendor/selenium-server-standalone-$SELENIUM_VERSION.jar \
+wget -nc -O vendor/selenium-server-standalone-$SELENIUM_VERSION.jar \
   https://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-$SELENIUM_VERSION.jar
