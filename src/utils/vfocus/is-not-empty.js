4cf100c8a0ff989c168c063ff3f8bc1607ec7fd0
Jon Parsons <jon.parsons@guardian.co.uk>
2014-12-09 10:13:23 +0000

    added description to is-not-empty fix

--- src/utils/vfocus/is-not-empty.js
+++ src/utils/vfocus/is-not-empty.js
@@ -8,5 +8,8 @@ module.exports = function isNotEmpty(focus) {
     throw new Error('Only a valid VFocus can be passed to isNotEmpty');
   }
 
+  //checking if an element is text prevents empty text elements
+  //containing elements like breaks being added to the dom when
+  //a note is created
   return isVText(focus) && !isEmpty(focus);
 };
