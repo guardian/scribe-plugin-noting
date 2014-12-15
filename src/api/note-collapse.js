89c03948e27c2d5734033453ff42193aabeade6f
Simon Hildrew <simon@hildrew.net>
2014-12-15 15:22:49 +0000

    Fix a typo that broke the collapse note toggle

--- src/api/note-collapse.js
+++ src/api/note-collapse.js
@@ -1,2 +1,2 @@
 exports.collapseToggleSelectedNote = require('../actions/noting/toggle-selected-note-collapse-state');
-exports.collapseToggleAllNote = require('../actions/noting/toggle-all-note-collapse-state');
+exports.collapseToggleAllNotes = require('../actions/noting/toggle-all-note-collapse-state');
