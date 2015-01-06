05f703f44bca47698d7f2820e79a1922ae0f48e8
Jon Parsons <jon.parsons@guardian.co.uk>
2015-01-06 17:58:06 +0000

    added fix for note toggles

--- src/actions/noting/toggle-note-classes.js
+++ src/actions/noting/toggle-note-classes.js
@@ -1,5 +1,8 @@
 var _ = require('lodash');
 var toggleClass = require('../vdom/toggle-class');
+var addClass = require('../vdom/add-class');
+var removeClass = require('../vdom/remove-class');
+var collapseState = require('../../utils/collapse-state');
 
 module.exports = function toggleNoteClasses(notes, className) {
 
@@ -10,8 +13,21 @@ module.exports = function toggleNoteClasses(notes, className) {
   notes = _.isArray(notes) ? notes : [notes];
   notes = _.flatten(notes);
 
+  var action;
+  if (notes.length === 1) {
+    //if we have only one note we can assume that it should be toggled
+    //because we assume it has been clicked
+    action = toggleClass;
+  } else {
+    //if we have more than one note then we want them all to share state
+    var state = collapseState.get();
+    state ? action = removeClass : action = addClass;
+    collapseState.set(!state);
+  }
+
   notes.forEach(function(vNode) {
-    toggleClass(vNode, className);
+    vNode = (vNode.vNode || vNode);
+    action(vNode, className);
   });
 
 };
