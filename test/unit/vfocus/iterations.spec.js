860565535b47534825950b3e6983dd7e06aa99b9
alessandro ludovici <alessandro.ludovici@guardian.com>
2015-01-12 13:00:49 +0000

    Added iteration.spec with basic structure to unit tests

---
+++ test/unit/vfocus/iterations.spec.js
@@ -0,0 +1,79 @@
+var path = require('path');
+var _ = require('lodash');
+var chai = require('chai');
+var expect = chai.expect;
+
+var h = require('virtual-hyperscript');
+var VText = require('vtree/vtext');
+var VFocus = require(path.resolve(process.cwd(), 'src/vfocus'));
+
+describe('VFocus - Iterations', function() {
+
+
+	describe('forEach', function() {
+
+		beforeEach(function() {
+
+		});
+
+
+		it('', function(){
+
+		});
+
+	});
+
+
+	describe('flatten', function() {
+
+		beforeEach(function() {
+
+		});
+
+
+		it('', function(){
+
+		});
+
+	});
+
+
+	describe('takeWhile', function() {
+
+		beforeEach(function() {
+
+		});
+
+		it('', function(){
+
+		});
+
+	});
+
+
+	describe('filter', function() {
+
+		beforeEach(function() {
+
+		});
+
+		it('', function(){
+
+		});
+
+	});
+
+
+	describe('find', function() {
+
+		beforeEach(function() {
+
+		});
+
+		it('', function(){
+
+		});
+
+	});
+
+});
