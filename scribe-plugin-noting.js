99acfe31b05f72a6163fdd8dd78e0e231f9371b0
Jon Parsons <jon.parsons@guardian.co.uk>
2015-01-15 15:57:43 +0000

    removed console log

--- scribe-plugin-noting.js
+++ scribe-plugin-noting.js
@@ -7,11 +7,6 @@ var generateNoteController = require('./src/generate-note-controller');
 //   scribeInstancesSelector: '.ui-rich-text-editor__input' }
 module.exports = function(config) {
   return function(scribe) {
-
-    console.log('-----------------------');
-    console.log('init', config);
-    console.log('-----------------------');
-
     generateNoteController(scribe, config);
   };
 };
