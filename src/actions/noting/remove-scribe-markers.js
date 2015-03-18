7de872a7685c62bc7c380b45b769f3556bf2a036
Robin Edman <robin.edman@guardian.co.uk>
2015-03-18 15:11:07 +0000

    Ensure removeScribeMarkers always run from the top of the tree

--- src/actions/noting/remove-scribe-markers.js
+++ src/actions/noting/remove-scribe-markers.js
@@ -2,13 +2,17 @@ var isVFocus = require('../../utils/vfocus/is-vfocus');
 var isScribeMarker = require('../../utils/noting/is-scribe-marker');
 var errorHandle = require('../../utils/error-handle');
 
-module.exports = function removeScribemarkers(focus) {
+/**
+ * Iterates through the entire tree from the top and removes all the scribe markers.
+ * @param  {VFocus} focus Any focus within the tree.
+ */
+module.exports = function removeScribeMarkers(focus) {
 
   if (!isVFocus(focus)) {
     errorHandle('Only a valid VFocus can be passed to removeScribeMarkers, you passed: %s', focus);
   }
 
-  focus.filter(isScribeMarker).forEach(function(marker) {
+  focus.top().filter(isScribeMarker).forEach(function(marker) {
     marker.remove();
   });
 
