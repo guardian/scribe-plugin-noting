bd43df18baa7899717fd2dede11c8d4dc2189c2d
Robin Edman <robin.edman@guardian.co.uk>
2015-05-22 12:59:37 +0100

    Changes the algorithm used in VFocus.prototype.prev()
    The algorithm used wasn't quite correct. In some cases, such as issue #127
    it'd skip nodes. Amended the test for prev() so it tests this case.

--- test/unit/vfocus/movements.spec.js
+++ test/unit/vfocus/movements.spec.js
@@ -101,7 +101,7 @@ describe('VFocus - Movements', function() {
     beforeEach(function() {
       leftTree = h('div', [
         h('p#first'),
-        h('p#last') 
+        h('p#last')
       ]);
 
       firstParagraph = leftTree.children[0];
@@ -135,7 +135,7 @@ describe('VFocus - Movements', function() {
         ]),
         h('p#4')
     ]);
-    
+
     firstNode = nextTree.children[0];
     secondNode = firstNode.children[0];
     thirdNode = firstNode.children[1];
@@ -159,40 +159,40 @@ describe('VFocus - Movements', function() {
    });
  });
 
+
   describe('prev()', function() {
 
-    var prevTreeFocus, prevTree, firstNode, secondNode, thirdNode, fourthNode;
+    var prevTreeFocus;
     beforeEach(function() {
-      prevTree = h(
-        'div', [
-          h('p#4', [
-            h('b#3'),
-            h('i#2')
+      var prevTree = h(
+        'div#sixth', [
+          h('p#fifth', [
+            h('gu-note#fourth', [
+              new VText('third')
+            ]),
+            new VText('second')
           ]),
-          h('p#1')
+          h('p#first', [
+            h('gu-note#start', [
+              new VText('Some noted text')
+            ]),
+            new VText('Some more text here')
+          ])
       ]);
-          
-      firstNode = prevTree.children[1];
-      fourthNode = prevTree.children[0];
-      secondNode = fourthNode.children[1];
-      thirdNode = fourthNode.children[0];
 
       prevTreeFocus = new VFocus(prevTree);
     });
 
-    it('focuses prev nodes in the right order', function() {
-      // go to the last element of the three
-      var firstPrevFocus = prevTreeFocus.next().next().next().next();
-      expect(firstPrevFocus.vNode).to.equal(firstNode);
+    it('focuses previous nodes in the right order', function() {
+      var startFocus = prevTreeFocus.find(f => f.vNode.properties && f.vNode.properties.id === 'start');
+      var steps = startFocus.takeWhile(f => f, 'prev');
 
-      var secondPrevFocus = firstPrevFocus.prev();
-      expect(secondPrevFocus.vNode).to.equal(secondNode);
-
-      var thirdPrevFocus = secondPrevFocus.prev();
-      expect(thirdPrevFocus.vNode).to.equal(thirdNode);
-
-      var fourthPrevFocus = thirdPrevFocus.prev();
-      expect(fourthPrevFocus.vNode).to.equal(fourthNode);
+      expect(steps[1].vNode.properties.id).to.equal('first');
+      expect(steps[2].vNode.text).to.equal('second');
+      expect(steps[3].vNode.text).to.equal('third');
+      expect(steps[4].vNode.properties.id).to.equal('fourth');
+      expect(steps[5].vNode.properties.id).to.equal('fifth');
+      expect(steps[6].vNode.properties.id).to.equal('sixth');
     });
   });
 
