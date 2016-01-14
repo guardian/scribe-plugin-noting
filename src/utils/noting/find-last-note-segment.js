e41696216916efa08f17b8c63975ce948602f912
Charles Szilagyi <charles@charlesagile.com>
2016-01-14 15:47:26 +0000

    Fix for #144 Notes incorrectly concatenated
    The notion of standalone note had to be introduces to be able to handle all use cases (noting on the left or right or inside other notes). With this hint `isWithinNote` and `createNoteFromSelection` work much more reliably.

--- src/utils/noting/find-last-note-segment.js
+++ src/utils/noting/find-last-note-segment.js
@@ -1,18 +1,18 @@
 var isVFocus = require('../vfocus/is-vfocus');
 var stillWithinNote = require('./still-within-note');
 var isNoteSegment = require('./is-note-segment');
+var isEmpty = require('../vfocus/is-empty');
 var errorHandle = require('../error-handle');
 var config = require('../../config');
 
-module.exports = function findLastNoteSegment(focus, tagName = config.get('defaultTagName')) {
+module.exports = function findLastNoteSegment(focus, tagName = config.get('defaultTagName'), isStandaloneNote = false) {
 
   if (!isVFocus(focus)) {
     errorHandle('only a valid VFocus can be passed to findFirstNoteSegment, you passed: %s', focus);
   }
 
   return focus
-    .takeWhile((node)=> stillWithinNote(node, tagName))
+    .takeWhile((node)=> stillWithinNote(node, tagName, isStandaloneNote))
     .filter((node)=> isNoteSegment(node, tagName))
     .splice(-1)[0];
-
 };
